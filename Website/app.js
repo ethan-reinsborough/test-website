18c321b7b8dd5477b7891b30ed4f8e7043b771a6
ethan-reinsborough <59565849+ethan-reinsborough@users.noreply.github.com>
2020-02-02 03:47:03 -0400

    Updates to Gacha Function

--- Website/app.js
+++ Website/app.js
@@ -1,3 +1,4 @@
+/*
 document.querySelector('button').addEventListener('click',function clickHandler(e){
 
     this.removeEventListener('click',clickHandler,false);
@@ -13,3 +14,26 @@ document.querySelector('button').addEventListener('click',function clickHandler(
     },4300);
 
 },false);
+
+
+
+function GACHA() {
+    result = Math.round(Math.random() * 100);
+    if (result < 50){
+        document.getElementById("t").innerHTML = result;
+        break;
+    }
+    if (result >= 50 && result <= 75){
+        document.getElementById("t").innerHTML = result;
+        break;
+    }
+    if (result > 75 && result <= 95){
+        document.getElementById("t").innerHTML = result;
+        break;
+    }
+    if (result > 95){
+        document.getElementById("t").innerHTML = result;
+        break;
+    }
+}
+*/
