fd71cd260e7d5d3b5a8819df8a943145c07afbfb
ethan-reinsborough <59565849+ethan-reinsborough@users.noreply.github.com>
2020-02-02 03:13:23 -0400

    Updated Index Page

---
+++ Website/app.js
@@ -0,0 +1,15 @@
+document.querySelector('button').addEventListener('click',function clickHandler(e){
+
+    this.removeEventListener('click',clickHandler,false);
+
+    e.preventDefault();
+    var self = this;
+    setTimeout(function(){
+        self.className = 'loading';
+    },125);
+
+    setTimeout(function(){
+        self.className = 'ready';
+    },4300);
+
+},false);
