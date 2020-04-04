381b70d5814bd8be172eb8e45b50c868dfe92dae
ethan-reinsborough <59565849+ethan-reinsborough@users.noreply.github.com>
2020-04-04 01:27:03 -0300

    Website Reformat
    Restructured website to add responsiveness and simplicity for future additions.

--- Website/app.js
+++ Website/app.js
@@ -37,3 +37,11 @@ function GACHA() {
     }
 }
 */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  } 
